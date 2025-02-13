1975b8bae8c1d70de2c46562e28ed17ee8849793
Coen <coenonderwater@gmail.com>
2025-02-13 19:41:01 +0100

    fix: engine types

--- src/engine/engine-factory.ts
+++ src/engine/engine-factory.ts
@@ -1,13 +1,13 @@
 import type { Engine } from "./engine";
 import { v0Engine } from "./v0.engine";
 import { v1Engine } from "./v1.engine";
+import { v1point2Engine } from "./v1point2.engine";
 
 export type EngineType = {
     name: string;
     version: string
     executor: Engine|null;
 }
-
 type EngineConstructor = new () => Engine;
 export type EngineOption = {
     name: string;
@@ -15,20 +15,21 @@ export type EngineOption = {
     construct: EngineConstructor|null
 }
 
-export const engines: { [key: string]: EngineOption } = {
-    human: { name: 'manual', version: 'human', construct: null },
-    v0: { name: 'v0-bogobot', version: 'v0', construct: v0Engine },
-    v1: { name: 'v1-greedybot', version: 'v1', construct: v1Engine }
-}
+export const engines = [
+    { name: 'manual', version: 'human', construct: null },
+    { name: 'v0-bogobot', version: 'v0', construct: v0Engine },
+    { name: 'v1-greedybot', version: 'v1', construct: v1Engine },
+    { name: 'v1.2-smarter-greedybot', version: 'v1.2', construct: v1point2Engine }
+];
 
 export class EngineFactory {
     static create(version: string): EngineType {
-      const engine = engines[version];
-      console.log(engine);
+      let engine = engines.find((engine) => engine.version === version);
+      if (!engine) engine = engines[0];
       return { name: engine.name, version, executor: engine.construct !== null ? new engine.construct() : null };
     }
 
-    static getEngines(): { [key: string]: EngineOption } {
+    static getEngines(): EngineOption[] {
       return engines;
     }
 }
